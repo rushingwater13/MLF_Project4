5d1b6bdf582d1ed66438a5cd7a884338c521882a
Emily <66390405+rushingwater13@users.noreply.github.com>
2026-05-06 20:45:51 +0000

    implement optimal, B6, and B7

--- policy.py
+++ policy.py
@@ -10,30 +10,25 @@ value_function = [
 
 gamma = 0.9
 
-def uniform():
+def calculate_function(mode):
 
     global value_function
 
     for i in range(10):
 
-        new_function = []
-        diff = []
-        for _ in range(5):
-            new_function.append([0] * 5)
-            diff.append([0] * 5)
+        new_function = [[0] * 5 for _ in range(5)]
+        diff = [[0] * 5 for _ in range(5)]
 
         for j in range(5):
             for k in range(5):
 
-                #print(f"j: {j}, k: {k}")
-
-                new_function[j][k] = get_update(j, k)
+                new_function[j][k] = get_update(j, k, mode)
                 diff[j][k] = new_function[j][k] - value_function[j][k]
 
         print(f"Iteration {i+1}:")
         print_grid(new_function)
 
-        print(f"Difference:")
+        print(f"Difference (new - old):")
         print_grid(diff)
 
         
@@ -41,10 +36,19 @@ def uniform():
         
 
 
-def get_update(j, k):
+def get_update(j, k, mode):
 
     #("up", "down", "left", "right")
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+
+    if mode == "B6":
+        B_reward = 6
+    elif mode == "B7":
+        B_reward = 7
+    else:
+        B_reward = 5
+
 
 
     # This accounts for state A jumping to A' 
@@ -53,9 +57,9 @@ def get_update(j, k):
         
     # This accounts for state B jumping to B' 
     elif j == 0 and k == 3: 
-       return gamma * value_function[2][3] + 5
+       return gamma * value_function[2][3] + B_reward
         
-
+    best_value = float('-inf')
     value = 0;
 
     # This is everyone else, accounting for walls
@@ -71,10 +75,15 @@ def get_update(j, k):
         else:
             reward = 0
 
-        
-        value += (gamma * value_function[nj][nk]) + reward
+        candidate = (gamma * value_function[nj][nk]) + reward
+        value += candidate
+        best_value = max(best_value, candidate)
 
-    return value / 4
+
+    if mode == "Uniform":
+        return value / 4
+    else:
+        return best_value
 
 
 def print_grid(grid):
@@ -85,9 +94,20 @@ def print_grid(grid):
     print()
 
 
+def reset_function():
+    return [[0] * 5 for _ in range(5)]
+
+
 def main():
 
-    uniform()
+    global value_function
+
+    policies = ["Uniform", "Optimal", "B6", "B7"]
+
+    for policy in policies:
+        value_function = reset_function()
+        print(f"-------- {policy} --------")
+        calculate_function(policy)
 
 if __name__ == "__main__":
     main()
