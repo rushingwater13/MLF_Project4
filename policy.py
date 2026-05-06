ea5cbdc1baade7f1a51828852ca23dc6480b512a
Emily <66390405+rushingwater13@users.noreply.github.com>
2026-05-06 20:19:05 +0000

    fix uniform

--- policy.py
+++ policy.py
@@ -1,59 +1,88 @@
 
-rewards = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 5, 0],
-    [0, 0, 0, 0, 0],
-    [0, 10, 0, 0, 0],
-]
 
-policy = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-]
+value_function = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
 
+gamma = 0.9
 
 def uniform():
-    new_policy = []
-    for i in range(6):
-        new_policy.append([])
 
-    for i in range(11):
-        for j in len(policy):
-            for k in len(policy[j]):
+    global value_function
 
-                new_value = 0.25 * update(j, k)
-                new_policy[j].append(new_value)
+    for i in range(10):
+
+        new_function = []
+        for _ in range(5):
+            new_function.append([0] * 5)
+
+        for j in range(5):
+            for k in range(5):
+
+                #print(f"j: {j}, k: {k}")
+
+                new_function[j][k] = get_update(j, k)
+
+        print(f"Iteration {i+1}:")
+        print_grid(new_function)
+        #print(f"Difference : {value_function - new_function}")
+
+        
+        value_function = new_function
+        
 
 
-        policy = new_policy
-        print(f"Iteration {i+1}: {policy}")
-
-
-def update(j, k):
+def get_update(j, k):
 
     #("up", "down", "left", "right")
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-    value = 0;
-    for h, w in directions:
-        if j + h < 0:
-            h += 1
-        elif j + h > 4:
-            h -= 1
 
-        if k + w < 0:
-            w += 1
-        elif k + w > 4:
-            w -= 1
+    # This accounts for state A jumping to A' 
+    if j == 0 and k == 1: 
+        return gamma * value_function[4][1] + 10
         
-        value += policy[j + h][k + w] + rewards[j + h][k + w]
+    # This accounts for state B jumping to B' 
+    elif j == 0 and k == 3: 
+       return gamma * value_function[2][3] + 5
+        
 
-    return value
+    value = 0;
+
+    # This is everyone else, accounting for walls
+    for h, w in directions:  
+
+        nj = j + h
+        nk = k + w 
+
+        if nj < 0 or nj > 4 or nk < 0 or nk > 4:
+            nj = j
+            nk = k
+            reward = -1
+        else:
+            reward = 0
+
+        
+        value += (gamma * value_function[nj][nk]) + reward
+
+    return value / 4
 
 
+def print_grid(grid):
+    
+    for row in grid:
+        print(" ".join(f"{val:6.2f}" for val in row))
 
-uniform()
+    print()
+
+
+def main():
+
+    uniform()
+
+if __name__ == "__main__":
+    main()
